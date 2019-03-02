61ec1ce47b9cd03ee8c87ecb7fc341210c9ca53d
CromixPT <tiago.g.candeias@gmail.com>
2019-03-02 20:19:34 +0000

    Add movement method

--- WorldLibrary/MovementProcessor.cs
+++ WorldLibrary/MovementProcessor.cs
@@ -1,7 +1,13 @@
-﻿namespace WorldLibrary
-{
-    class MovementProcessor
-    {
+﻿using System;
+using WorldLibrary.Models;
 
+namespace WorldLibrary
+{
+    public class MovementProcessor
+    {
+        internal static PositionModel Movement(PositionModel currentPosition, char letter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
