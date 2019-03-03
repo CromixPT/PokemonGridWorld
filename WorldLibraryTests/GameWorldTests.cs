2026c193844ca7437231b3a0956d9cbcc55fbff0
CromixPT <tiago.g.candeias@gmail.com>
2019-03-03 11:05:29 +0000

    Removed character validation test methods

--- WorldLibraryTests/GameWorldTests.cs
+++ WorldLibraryTests/GameWorldTests.cs
@@ -6,39 +6,6 @@ namespace WorldLibraryTests
     public class GameWorldTests
     {
 
-        #region isCharacterValid_Testing
-        [Theory]
-        [InlineData('N')]
-        [InlineData('S')]
-        [InlineData('O')]
-        [InlineData('E')]
-        public void isCharacterValid_ValidChars_ShouldPass(char input)
-        {
-            bool actual = StringProcessor.isCharacterValid(input);
-
-            Assert.True(actual);
-        }
-
-
-
-        [Theory]
-        [InlineData('F')]
-        [InlineData('R')]
-        [InlineData('Q')]
-        [InlineData('P')]
-        [InlineData('1')]
-        [InlineData('/')]
-        public void isCharacterValid_InvalidChars_ShouldFail(char input)
-        {
-            bool actual = StringProcessor.isCharacterValid(input);
-
-            Assert.False(actual);
-        }
-        #endregion
-
-
-
-
         [Theory]
         [InlineData("E", 2)]
         [InlineData("NESO", 4)]
